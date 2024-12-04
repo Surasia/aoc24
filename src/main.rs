0818a65f4278029c4d14051a77f187b0c51ed098
Surasia <74399067+Surasia@users.noreply.github.com>
2024-12-04 13:08:09 +0100

    aoc24: day 4
    a bit late on this

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use day1::{day1_p1, day1_p2};
 use day2::{day2_p1, day2_p2};
 use day3::{day3_p1, day3_p2};
+use day4::{day4_p1, day4_p2};
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("--- Day 1 ---");
@@ -16,4 +18,7 @@ fn main() {
     println!("--- Day 3 ---");
     day3_p1();
     day3_p2();
+    println!("--- Day 4 ---");
+    day4_p1();
+    day4_p2();
 }
